29b1e1e7744359c9bc838605cbae1f7eeb73ca5f
wwwxxch <chihhui.x@gmail.com>
2025-03-16 19:53:17 +0800

    change the prompt

--- src/genai/get_ai_response.py
+++ src/genai/get_ai_response.py
@@ -31,36 +31,49 @@ def llm_provider_factory():
         raise e
 
 
-def create_cat_health_chain(llm):
-    cat_data = {
-        "name": "花花",
-        "birth_year": 2007,
-        "sex": "male",
-        "species": "cat",
-        "breed": "feline domestic shorthair",
-        "medical_history": [
-            "Feline Hypertrophic Cardiomyopathy Phase IIb, examined on 2024 June",
-            "Hyperthyroidism, but the unilateral total thyroid gland has been removed at end of 2023",
-        ],
-    }
+cat_data = {
+    "name": "花花",
+    "birth_year": 2007,
+    "sex": "male",
+    "species": "cat",
+    "breed": "feline domestic shorthair",
+    "medical_history": [
+        "Feline Hypertrophic Cardiomyopathy Phase IIb, examined on 2024 June",
+        "Hyperthyroidism, but the unilateral total thyroid gland has been removed at end of 2023",
+    ],
+}
+
+
+def create_cat_health_chain(llm, specific):
     current_dt = datetime.now()
     current_y = current_dt.year
-    system_prompt = f"""
-    You are an AI assistant specialized in cat health care for {cat_data["name"]}
 
-    Cat-Specific Information:
-        - Name: {cat_data['name']}
-        - Birth Year: {cat_data['birth_year']}
-        - Basic Information: sex is {cat_data['sex']}, breed is {cat_data['breed']}
-        - Medical History: {', '.join(cat_data['medical_history'])}
-
-    Guidelines:
-        - Only answer questions related to cats
-        - For questions about {cat_data["name"]}, use his specific information to answer
-        - Answer the questions in 繁體中文
-        - If a question is not cat-related, respond: "這與貓咪照護無關，我無法回答"
-    Current year is {current_y}
-    """
+    # System prompt
+    if specific:
+        system_prompt = f"""
+            You are an AI assistant specialized in cat health care for {cat_data["name"]}
+            Cat-Specific Information:
+                - Name: {cat_data['name']}
+                - Birth Year: {cat_data['birth_year']}
+                - Basic Information: sex is {cat_data['sex']}, breed is {cat_data['breed']}
+                - Medical History: {', '.join(cat_data['medical_history'])}
+            Guidelines:
+                - Identify whether the question is related to cat and is related to the specific
+                    cat {cat_data["name"]}, if no, answer as a general cat health expert without
+                    mentioning specific cats.
+                - For questions about {cat_data["name"]}, use his specific information to answer
+                - Answer the questions in 繁體中文
+                - If a question is not cat-related, respond: "這與貓咪照護無關，我無法回答"
+            Current year is {current_y}
+        """
+    else:
+        system_prompt = """
+            You are an AI assistant specialized in cat health care
+            Guidelines:
+                - Only answer questions related to cats
+                - Answer the questions in 繁體中文
+                - If a question is not cat-related, respond: "這與貓咪照護無關，我無法回答"
+        """
 
     # Create prompt template
     prompt = ChatPromptTemplate.from_messages([("system", system_prompt), ("human", "{input}")])
@@ -73,7 +86,12 @@ def create_cat_health_chain(llm):
 def get_ai_response(user_input):
     try:
         llm = llm_provider_factory()
-        cat_health_chain = create_cat_health_chain(llm)
+
+        if "花花" in user_input:
+            cat_health_chain = create_cat_health_chain(llm, specific=True)
+        else:
+            cat_health_chain = create_cat_health_chain(llm, specific=False)
+
         response = cat_health_chain.invoke({"input": user_input})
 
     except Exception as e:
